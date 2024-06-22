f45b39d9c98448ff163eec92ac1598acf22c6a53
David Alves Dutra <dadutra@hotmail.com>
2024-06-22 09:33:43 -0300

    Test application with H2 db. OK.

--- src/main/java/com/demo/web_services/Entities/Users.java
+++ src/main/java/com/demo/web_services/Entities/Users.java
@@ -1,10 +1,17 @@
 package com.demo.web_services.Entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Users implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  long id;
     private String name;
     private String email;
