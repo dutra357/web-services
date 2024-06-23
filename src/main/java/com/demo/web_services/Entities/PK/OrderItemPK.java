347606805e682d01b3aa61364565e1936f4d8a95
David Alves Dutra <dadutra@hotmail.com>
2024-06-23 10:04:08 -0300

    OrderItemPK class implemented. JoinColumn many to one.

---
+++ src/main/java/com/demo/web_services/Entities/PK/OrderItemPK.java
@@ -0,0 +1,61 @@
+package com.demo.web_services.Entities.PK;
+
+import com.demo.web_services.Entities.Orders;
+import com.demo.web_services.Entities.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class OrderItemPK implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Orders order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+
+
+    public Orders getOrder() {
+        return order;
+    }
+
+    public void setOrder(Orders order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemPK that = (OrderItemPK) o;
+        return Objects.equals(getOrder(), that.getOrder()) && Objects.equals(getProduct(), that.getProduct());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getOrder(), getProduct());
+    }
+
+    @Override
+    public String toString() {
+        return "OrderItemPK{" +
+                "order=" + order +
+                ", product=" + product +
+                '}';
+    }
+}
